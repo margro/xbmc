72a7ff35e1177be662cda4965fcd542d4ea71fe4
Marcel Groothuis <margro.xbmc@gmail.com>
2016-08-23 17:46:16 +0200

    Add some convenience scripts to prepare the VS2015 build environment

---
+++ project/1_builddeps.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SET PATH=%PATH%;C:\Program Files (x86)\CMake\bin
+
+cd %~dp0\..
+SET KODI_SRC=%CD%
+SET SOLUTION_DIR=%KODI_SRC%\project\VS2015
+
+cd %KODI_SRC%\project\BuildDependencies
+call DownloadBuildDeps.bat
+call DownloadMingwBuildEnv.bat
+
+cd %KODI_SRC%\tools\buildsteps\win32
+call make-mingwlibs.bat noclean
---
+++ project/2_generate_vs2015.bat
@@ -0,0 +1,19 @@
+@echo off
+
+SET PATH=%PATH%;C:\Program Files (x86)\CMake\bin
+
+cd %~dp0\..
+SET KODI_SRC=%CD%
+SET SOLUTION_DIR=%KODI_SRC%\project\VS2015
+
+IF NOT EXIST %SOLUTION_DIR% (
+	mkdir %SOLUTION_DIR%
+)
+
+cd %SOLUTION_DIR%
+
+cmake -G "Visual Studio 14" %KODI_SRC%\project\cmake
+
+set KODI_HOME=%SOLUTION_DIR%
+set PATH=%SOLUTION_DIR%\system;%PATH%
+start %SOLUTION_DIR%\kodi.sln
